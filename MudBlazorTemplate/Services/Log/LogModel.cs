ce314b51335f2471e9b579b3f7591958040691dd
Michael Hillman <mjhillman@msn.com>
2023-12-23 17:28:11 -0500

    Added Sqlite logging

---
+++ MudBlazorTemplate/Services/Log/LogModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MudBlazorTemplate.Services
+{
+    public class LogModel
+    {
+        public long Id { get; set; }
+        public string Ip { get; set; }
+        public string Message { get; set; }
+        public DateTime MessageDate { get; set; }
+        public LogModel() { }
+    }
+}
---
+++ MudBlazorTemplate/Services/Log/LogService.cs
@@ -0,0 +1,38 @@
+﻿using HITS.LIB.Sqlite;
+
+namespace MudBlazorTemplate.Services
+{
+    public class LogService
+    {
+        public static async Task<List<LogModel>> GetLog(string connectionString)
+        {
+            using (SqlAsync dal = new SqlAsync())
+            {
+                string sql = $"SELECT * FROM Log WHERE MessageDate > '{DateTime.Now.AddMonths(-1)}' ORDER BY MessageDate DESC;";
+                return await dal.GetDataTableAsListAsync<LogModel>(connectionString, sql).ConfigureAwait(true);
+            }
+        }
+
+        public static async Task<int> InsertLogData(string connectionString, string message, string ip = "")
+        {
+            using (SqlAsync dal = new SqlAsync())
+            {
+                var parameters = new Dictionary<string, object> { { "$message", message } };
+                string sql = $"INSERT INTO LOG (Ip, Message, MessageDate) VALUES ('{ip}', $message, '{DateTime.Now}')";
+                return await dal.ExecuteNonQueryAsync(connectionString, sql, parameters).ConfigureAwait(true);
+            }
+        }
+
+        public static async Task<int> DeleteLog(string connectionString)
+        {
+            using (SqlAsync dal = new SqlAsync())
+            {
+                string sql = $"DELETE FROM Log;";
+                return await dal.ExecuteNonQueryAsync(connectionString, sql).ConfigureAwait(true);
+            }
+        }
+
+        public LogService() { }
+
+    }
+}
